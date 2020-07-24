d831c73ee095aff017d2729a08df67e663d5cec0
hiyanxu <yanxu01@baijiahulian.com>
2020-07-24 14:06:15 +0800

    feat(Sum.go): 数组和迭代

--- array_slice/Sum.go
+++ array_slice/Sum.go
@@ -4,11 +4,80 @@ package array_slice
 两种初始化数组的方式：
 1、[5]{1, 2, 3, 4, 5}
 2、[...]{1, 2, 3, 4, 5}
+
+for循环遍历数组：
+	通过传统for循环可以遍历数组
+range遍历数组：
+	range会返回迭代数组，且返回数组元素的索引和值：key, value := range array
+
+数组类型：
+	数组的长度也是类型的一部分，不同长度的数组之间不可进行赋值
+
+重构：
+1、通过range遍历数组更加简洁。
 */
 func Sum(numbers [5]int) int {
 	sum := 0
+
+	// 采用for循环
+	//for i := 0; i < 5; i++ {
+	//	sum += numbers[i]
+	//}
+
+	// 重构1：采用range array
 	for _, num := range numbers {
 		sum += num
+	}
+
+	return sum
+}
+
+/**
+
+ */
+func SumBySlice(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+
+	return sum
+}
+
+/**
+make([]int, len)：
+	（1）切片需要通过make函数进行初始化后才可以使用；
+	（2）使用append()动态增加，不通过sum[i]的方式
+	未初始化直接使用会报错：panic: runtime error: index out of range [0] with length 0
+*/
+func SumAll(numbersToSum ...[]int) (sum []int) {
+	len := len(numbersToSum)
+	sum = make([]int, len)
+
+	for i := 0; i < len; i++ {
+		//sumOfNum := SumBySlice(numbersToSum[i])
+		//sum = append(sum, sumOfNum)
+		sum[i] = SumBySlice(numbersToSum[i])
+	}
+
+	return
+}
+
+/**
+可以使用slice[low:high]获取部分切片
+（1）包括左边不包括右边（若右边没有具体指定哪里结束，则包括最后） 长度：high - low  容量：底层数组长度 - low
+*/
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sum []int
+	length := len(numbersToSum)
+
+	for i := 0; i < length; i++ {
+		if len(numbersToSum[i]) == 0 {
+			sum = append(sum, 0)
+		} else {
+			tail := numbersToSum[i][1:]
+			sum = append(sum, SumBySlice(tail))
+		}
 	}
 
 	return sum
--- array_slice/Sum_test.go
+++ array_slice/Sum_test.go
@@ -1,13 +1,75 @@
 package array_slice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
+/**
+测试覆盖率工具：
+	通过运行go test -cover Sum_test.go Sum.go  可以进行测试覆盖率计算
+*/
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		t.Errorf("got %d, but want %d, %v", got, want, numbers)
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d, but want %d, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := SumBySlice(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d, but want %d, %v", got, want, numbers)
+		}
+	})
+}
+
+//func TestSum2(t *testing.T) {
+//	numbers := [4]int{1, 2, 3, 4}
+//
+//	// 报错 数组长度也是类型的一部分：cannot use numbers (type [4]int) as type [5]int in argument to Sum
+//	got := Sum(numbers)
+//	want := 10
+//	if got != want {
+//		t.Errorf("got %d, but want %d, %v", got, want, numbers)
+//	}
+//}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// golang不能对切片使用等号运算符，切片只能和nil做比较
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	// 由于checkSums函数限制了got、want的类型，所以传入别的类型时，会编译期报错
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
 }
